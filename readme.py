ae1956a52ec9c233457a8251f97f4298fdc871e4
bmqy <bmqy@qq.com>
2022-08-10 18:52:01 +0800

    更新文章标题； 更新readme；

--- readme.py
+++ readme.py
@@ -15,7 +15,8 @@ def get_posts(feed_url):
     feed_entries_length = len(feed_entries)
     all_number = feed_entries_length
 
-    for entrie in feed_entries[0: all_number]:
+    for i in range(all_number, 0, -1):
+        entrie = feed_entries[i]
         title = entrie["title"]
         link = entrie["url"]
         result = result + "\n" + "[" + title + "](" + link + ")" + "\n"
