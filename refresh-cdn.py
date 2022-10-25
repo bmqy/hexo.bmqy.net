e951a82f07efbd446272e22115d9b4ea3f0f3360
bmqy <bmqy@qq.com>
2022-10-25 16:41:44 +0800

    刷新cdn

--- refresh-cdn.py
+++ refresh-cdn.py
@@ -7,14 +7,16 @@ from tencentcloud.common.exception.tencent_cloud_sdk_exception import (
     TencentCloudSDKException,
 )
 from tencentcloud.cdn.v20180606 import cdn_client, models
-# 传入参数
+
 parser = argparse.ArgumentParser(description='-i <secretId> -k <secretKey>')
-parser.add_argument('-i', '--secretid', type=str, required=True, help='secretId')
-parser.add_argument('-k', '--secretkey', type=str, required=True, help='secretKey')
+parser.add_argument('-i', '--secretid', type=str,
+                    required=True, help='secretId')
+parser.add_argument('-k', '--secretkey', type=str,
+                    required=True, help='secretKey')
 args = parser.parse_args()
 
 try:
-    cred = credential.Credential(args.secretid,args.secretkey)
+    cred = credential.Credential(args.secretid, args.secretkey)
     httpProfile = HttpProfile()
     httpProfile.endpoint = "cdn.tencentcloudapi.com"
 
