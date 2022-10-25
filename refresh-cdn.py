1c70c3ffa71656505bdc0fb9fa5500740fe1c17d
bmqy <bmqy@qq.com>
2022-10-25 13:53:27 +0800

    刷新cdn

--- refresh-cdn.py
+++ refresh-cdn.py
@@ -7,9 +7,7 @@ from tencentcloud.common.exception.tencent_cloud_sdk_exception import (
     TencentCloudSDKException,
 )
 from tencentcloud.cdn.v20180606 import cdn_client, models
-
 # 传入参数
-
 parser = argparse.ArgumentParser(description='-i <secretId> -k <secretKey>')
 parser.add_argument('-i', '--secretid', type=str, required=True, help='secretId')
 parser.add_argument('-k', '--secretkey', type=str, required=True, help='secretKey')
