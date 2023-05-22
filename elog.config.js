3adf40be4b649d30e484ab12c644b8368fc6b4ca
bmqy <bmqy@qq.com>
2023-05-22 13:31:48 +0800

    update

--- elog.config.js
+++ elog.config.js
@@ -21,7 +21,7 @@ module.exports = {
     platform: 'local',
     local: {
       outputDir: '/source/_posts',
-      filename: 'title',
+      filename: 'urlname',
       format: 'matter-markdown',
       catalog: false,
       formatExt: 'elog.format.js',
